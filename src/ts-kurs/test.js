0781628e7052aba56537dd91d3596bfb514792b9
MateuszPospiech <pospiechmateusz@gmail.com>
2019-07-18 21:56:21 +0200

    TypeScript types

---
+++ src/ts-kurs/test.js
@@ -0,0 +1,11 @@
+var TestType = /** @class */ (function () {
+    function TestType() {
+        this.name = 'Mateusz';
+        this.age = 27;
+        this.stack = ['html', 'css', 'js'];
+        this.dates = [new Date(), new Date()];
+        this.pairs = ['pushups', 30];
+        this.xyz = 222;
+    }
+    return TestType;
+}());
---
+++ src/ts-kurs/test.ts
@@ -0,0 +1,9 @@
+class TestType{
+
+    name: string = 'Mateusz';
+    age: number = 27;
+    stack: string[] = ['html', 'css', 'js'];
+    dates: Array<Date> = [new Date(), new Date()];
+    pairs: [string, number] = ['pushups', 30];
+    xyz: any = 222;
+}
