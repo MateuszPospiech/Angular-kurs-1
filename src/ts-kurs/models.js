2b900d6a97d2d8ae34a7ca4445454765c1085aee
MateuszPospiech <pospiechmateusz@gmail.com>
2019-07-21 13:56:24 +0200

    TypeScript moduls, imports

---
+++ src/ts-kurs/models.js
@@ -0,0 +1,17 @@
+"use strict";
+exports.__esModule = true;
+var One = /** @class */ (function () {
+    function One() {
+        console.log('class one');
+    }
+    return One;
+}());
+exports.One = One;
+var Two = /** @class */ (function () {
+    function Two() {
+        console.log('clas two');
+    }
+    return Two;
+}());
+exports.Two = Two;
+exports.PI = 3.14;
---
+++ src/ts-kurs/models.ts
@@ -0,0 +1,13 @@
+export class One {
+    constructor() {
+        console.log('class one');
+    }
+}
+
+export class Two {
+    constructor() {
+        console.log('clas two');
+    }
+}
+
+export const PI = 3.14;
